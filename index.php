ff8038c50a250c21256b264379a0358cff9337f8
arment4 <armentstudios@gmail.com>
2016-02-13 15:28:42 -0700

    índex estructura

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-
+  require_once 'incluir/configuracion.php';
+  include_once 'incluir/iniciohtml.php';
+  include 'header.php';
  ?>
   <body>
+    
 
+  <?php
+    include 'incluir/footer.php';
+    include 'incluir/finalhtml.php';
+  ?>
   </body>
 
 
